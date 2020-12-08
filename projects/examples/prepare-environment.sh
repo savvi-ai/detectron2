5a59f3a4abce389a74a76aec146735703f69b36d
ryooit <ryooit70@gmail.com>
2020-12-08 14:33:56 +0900

    Delete unused line

--- projects/examples/prepare-environment.sh
+++ projects/examples/prepare-environment.sh
@@ -1,5 +1,2 @@
-pip install pyyaml==5.1 'pycocotools>=2.0.1'
 pip install --upgrade detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/torch1.6/index.html
-
-pip install opencv-python
 pip install savvihub
